b1cc52d792d3cc14ac532517858d731a2c965f9b
MasatakaSugita <sugita-m@sumarch.co.jp>
2023-01-09 12:52:34 +0900

    リファクタ

--- module/elb/main.tf
+++ module/elb/main.tf
@@ -35,45 +35,30 @@ resource "aws_security_group" "main" {
 
   vpc_id = var.vpc_id
 
+  ingress {
+    from_port = 80
+    protocol  = "tcp"
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    protocol  = "tcp"
+    to_port   = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.app_name}-alb"
   }
-}
-
-resource "aws_security_group_rule" "out" {
-  security_group_id = aws_security_group.main.id
-
-  type = "egress"
-
-  from_port = 0
-  to_port   = 0
-  protocol  = "-1"
-
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "http" {
-  security_group_id = aws_security_group.main.id
-
-  type = "ingress"
-
-  from_port = 80
-  to_port   = 80
-  protocol  = "tcp"
-
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "https" {
-  security_group_id = aws_security_group.main.id
-
-  type = "ingress"
-
-  from_port = 443
-  to_port   = 443
-  protocol  = "tcp"
-
-  cidr_blocks = ["0.0.0.0/0"]
 }
 
 #----------------------------
